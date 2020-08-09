c48fdadeec9e28b4e66f296b761c38d9e11bb42e
Matt Nemitz <matt.nemitz@gmail.com>
2020-08-09 20:31:32 +0200

    Adjust section height for portrait

--- src/activity/PaletteCard.svelte
+++ src/activity/PaletteCard.svelte
@@ -4,34 +4,35 @@
 	import Palette from './Palette.svelte';
 
 	export let colors = [];
-
-	$: {
-			console.warn(colors);
-	}
 </script>
 <style>
-* :global(.palette-card-paper) {
-    display: flex;
-    flex-direction: column;
-		padding: 0;
-		height: 100%;
-		width: 100%;
-}
-#header {
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-}
-#button-container {
-	display: flex;
-	flex-direction: row;
-	justify-content: space-evenly;
-}
+	* :global(.palette-card-paper) {
+			display: flex;
+			flex-direction: column;
+			padding: 0;
+			height: 100%;
+			width: 100%;
+	}
+	#header {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	#button-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+	}
+	#palette-title {
+		padding: 5px;
+	}
 </style>
 <div class="container">
     <Paper class="palette-card-paper">
 				<div id="header">
-					<Title>Palette</Title>
+					<div id="palette-title">
+						<Title>Palette</Title>
+					</div>
 					<div id="button-container">
 						<IconButton class="material-icons" on:click={() => colors = []}>
 								delete
